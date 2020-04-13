d1277de433a028b0f61df95512ce112d63198bc0
Nilufer Tokdemir <nilufer.tokdemir@faradaynetworks.xyz>
2020-04-14 00:10:02 +0300

    added empty array

---
+++ Downloads/AOST-UserSide/model/kullaniciIstek.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+var kullaniciIstekSchema = new mongoose.Schema({
+    plaka: String,
+    hız: String,
+    mesafe: String,
+    tarih: Date
+});
+
+var KullaniciIstek=mongoose.model('KullaniciIstek', kullaniciIstekSchema);
+module.exports=KullaniciIstek;
